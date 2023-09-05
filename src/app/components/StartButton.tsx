bc96283b44cff623311795ae1233921af0d9d82f
Sijita <simonjt.s2001@gmail.com>
2023-09-05 10:09:10 -0500

    FEAT: Start button

---
+++ src/app/components/StartButton.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { Button } from "@nextui-org/react";
+import useStartGame from "../hooks/useStartGame";
+
+export default function StartButon() {
+  const { handleClick } = useStartGame();
+  return <Button onClick={handleClick}>Empezar!</Button>;
+}
