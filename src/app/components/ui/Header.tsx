5d7b16b1159fb774e3f52645e71e3c101e6fb533
Sijita <simonjt.s2001@gmail.com>
2023-09-05 16:26:50 -0500

    FIX: Header title

--- src/app/components/ui/Header.tsx
+++ src/app/components/ui/Header.tsx
@@ -5,7 +5,7 @@ import ThemeSwitcher from "./ThemeSwitcher";
 export default function Header() {
   return (
     <header className="flex flex-col gap-5">
-      <h1 className="text-3xl font-bold">Football Quizz</h1>
+      <h1 className="text-3xl font-bold">Football Quiz</h1>
       {/* <ThemeSwitcher /> */}
     </header>
   );
