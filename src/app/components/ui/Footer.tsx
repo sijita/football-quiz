2f9f8cb2246ae746f88ab067b83cfb483c1912fa
Sijita <simonjt.s2001@gmail.com>
2023-09-05 16:28:08 -0500

    FEAT: Footer

---
+++ src/app/components/ui/Footer.tsx
@@ -0,0 +1,41 @@
+import { Button, ButtonGroup, Divider } from "@nextui-org/react";
+import { HiArrowCircleLeft, HiArrowCircleRight } from "react-icons/hi";
+import { type Question as QuestionType } from "@/app/types";
+
+export default function Footer({
+  questions,
+  currentQuestion,
+  goToNextQuestion,
+  goToPreviousQuestion,
+  reset,
+}: {
+  questions: QuestionType[];
+  currentQuestion: number;
+  goToNextQuestion: () => void;
+  goToPreviousQuestion: () => void;
+  reset: () => void;
+}) {
+  return (
+    <div className="flex flex-col gap-5">
+      <ButtonGroup variant="flat" fullWidth>
+        <Button
+          onClick={goToPreviousQuestion}
+          isDisabled={currentQuestion === 0}
+        >
+          <HiArrowCircleLeft size={20} />
+          Anterior
+        </Button>
+        <Divider orientation="vertical" />
+        <Button
+          onClick={goToNextQuestion}
+          isDisabled={currentQuestion === questions.length - 1}
+        >
+          Siguiente <HiArrowCircleRight size={20} />
+        </Button>
+      </ButtonGroup>
+      <Button onClick={reset} variant="ghost" fullWidth>
+        Resetear juego
+      </Button>
+    </div>
+  );
+}
