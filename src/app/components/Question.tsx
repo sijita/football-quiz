f141c79039028582c8deb262740542ae92cb2549
Sijita <simonjt.s2001@gmail.com>
2023-09-05 16:26:40 -0500

    FIX: Card size

--- src/app/components/Question.tsx
+++ src/app/components/Question.tsx
@@ -1,10 +1,11 @@
+"use client";
 import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
 import { type Question as QuestionType } from "../types";
 import ListBox from "./ui/ListBox";
 
 export default function Question({ info }: { info: QuestionType }) {
   return (
-    <Card className="max-w-[400px] rounded-md">
+    <Card className="max-w-[400px] min-w-[400px]">
       <CardHeader className="p-5">
         <h5 className="font-semibold">{info.question}</h5>
       </CardHeader>
