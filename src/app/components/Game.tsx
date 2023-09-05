acb91e069c61e42d8fe3f6897bbc36e460a1ff8b
Sijita <simonjt.s2001@gmail.com>
2023-09-05 16:27:58 -0500

    FIX: Hydratation error, useQuestionData hook - FEAT: Loading State

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,18 +1,65 @@
 "use client";
 import StartButon from "./StartButton";
-import { useQuestionsStore } from "../store/questions";
 import Question from "./Question";
+import Footer from "./ui/Footer";
+import Nav from "./ui/Nav";
+import useQuestionData from "../hooks/useQuestionData";
+import { useEffect, useState } from "react";
+import { Spinner } from "@nextui-org/react";
 
 export default function Game() {
-  const questions = useQuestionsStore((state) => state.questions);
-  const currentQuestion = useQuestionsStore((state) => state.currentQuestion);
+  const [loading, setLoading] = useState(true);
+  const {
+    questions,
+    currentQuestion,
+    questionInfo,
+    goToNextQuestion,
+    goToPreviousQuestion,
+    correct,
+    incorrect,
+    unanswered,
+    reset,
+  } = useQuestionData();
 
-  const questionInfo = questions[currentQuestion];
+  useEffect(() => {
+    setLoading(false);
+  }, []);
 
   return (
-    <div>
-      {!questions.length && <StartButon />}
-      {questions.length > 0 && <Question info={questionInfo} />}
+    <div className="flex flex-col gap-10">
+      {loading ? (
+        <div className="flex gap-5 items-center animate-pulse">
+          <p className="text-2xl">Cargando...</p>
+          <Spinner color="default" size="sm" />
+        </div>
+      ) : (
+        <>
+          {!questions.length && <StartButon />}
+          {questions.length > 0 && (
+            <>
+              <Nav
+                {...{
+                  questions,
+                  currentQuestion,
+                  correct,
+                  incorrect,
+                  unanswered,
+                }}
+              />
+              <Question info={questionInfo} />
+              <Footer
+                {...{
+                  questions,
+                  currentQuestion,
+                  goToNextQuestion,
+                  goToPreviousQuestion,
+                  reset,
+                }}
+              />
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 }
