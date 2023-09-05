145a7ae95c6ca9c328742879fec854d70c2a7910
Sijita <simonjt.s2001@gmail.com>
2023-09-05 10:08:53 -0500

    FEAT: Header

---
+++ src/app/components/ui/Header.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Divider } from "@nextui-org/react";
+import ThemeSwitcher from "./ThemeSwitcher";
+
+export default function Header() {
+  return (
+    <header className="flex flex-col gap-5">
+      <h1 className="text-3xl font-bold">Football Quizz</h1>
+      {/* <ThemeSwitcher /> */}
+    </header>
+  );
+}
